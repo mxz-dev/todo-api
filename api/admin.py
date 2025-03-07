d3c7e53a32284ad1536324784f69717a1783c834
mxz-dev <zabetzadehmohmmad@gmail.com>
2025-03-07 19:38:49 +0330

    stage

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Todo)
+class TodoAdmin(admin.ModelAdmin):
+    pass
--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+USER = get_user_model()
+
+class Todo(models.Model):
+    author = models.ForeignKey(USER, on_delete=models.CASCADE)
+    task = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    marked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.task} - {self.author}'
+    
+    class Meta:
+        verbose_name_plural = 'todo'
